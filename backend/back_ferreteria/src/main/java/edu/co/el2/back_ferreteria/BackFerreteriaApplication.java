9ceca4e1bf91ceea2a1529f153db0d4f1fd8e6c8
jdCarrillo15 <juandcarrillop15@gmail.com>
2025-06-07 17:05:11 -0500

    first commit

---
+++ backend/back_ferreteria/src/main/java/edu/co/el2/back_ferreteria/BackFerreteriaApplication.java
@@ -0,0 +1,13 @@
+package edu.co.el2.back_ferreteria;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackFerreteriaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackFerreteriaApplication.class, args);
+	}
+
+}
---
+++ backend/back_ferreteria/src/test/java/edu/co/el2/back_ferreteria/BackFerreteriaApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.co.el2.back_ferreteria;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackFerreteriaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
