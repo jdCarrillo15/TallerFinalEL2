1327aae57354a28245792cfdddcf8527f3cefdfe
jdCarrillo15 <juandcarrillop15@gmail.com>
2025-06-08 23:28:34 -0500

    Inicio de modelado de POJOS

---
+++ backend/back_ferreteria/src/main/java/edu/co/el2/back_ferreteria/model/Brand.java
@@ -0,0 +1,39 @@
+package edu.co.el2.back_ferreteria.model;
+
+public class Brand {
+
+    private short id_marca;
+    private String nombre_marca;
+
+    public Brand() {
+    }
+
+    public Brand(short id_marca, String nombre_marca) {
+        this.id_marca = id_marca;
+        this.nombre_marca = nombre_marca;
+    }
+
+    public short getId_marca() {
+        return id_marca;
+    }
+
+    public void setId_marca(short id_marca) {
+        this.id_marca = id_marca;
+    }
+
+    public String getNombre_marca() {
+        return nombre_marca;
+    }
+
+    public void setNombre_marca(String nombre_marca) {
+        this.nombre_marca = nombre_marca;
+    }
+
+    @Override
+    public String toString() {
+        return "Brand{"
+                + "id_marca=" + id_marca
+                + ", nombre_marca='" + nombre_marca + '\''
+                + '}';
+    }
+}
---
+++ backend/back_ferreteria/src/main/java/edu/co/el2/back_ferreteria/model/Provider.java
@@ -0,0 +1,53 @@
+package edu.co.el2.back_ferreteria.model;
+
+public class Provider {
+
+    private String codigo;
+    private String nombre;
+    private String telefono;
+
+    public Provider() {
+    }
+
+    public Provider(String codigo, String nombre, String telefono) {
+        this.codigo = codigo;
+        this.nombre = nombre;
+        this.telefono = telefono;
+    }
+
+    public String getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(String codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("Provider{");
+        sb.append("codigo=").append(codigo);
+        sb.append(", nombre=").append(nombre);
+        sb.append(", telefono=").append(telefono);
+        sb.append('}');
+        return sb.toString();
+    }
+
+}
