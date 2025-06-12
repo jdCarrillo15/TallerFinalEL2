36f3e113d196c1a9828429fdd7a6d8fd7eea668e
jdCarrillo15 <juandcarrillop15@gmail.com>
2025-06-11 19:27:38 -0500

    front ajustado

--- backend/back_ferreteria/src/main/java/edu/co/el2/back_ferreteria/Concept/ConceptRepository.java
+++
@@ -1,9 +0,0 @@
-package edu.co.el2.back_ferreteria.Concept;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ConceptRepository extends JpaRepository<ConceptEntity, Integer> {
-    
-}
--- backend/back_ferreteria/src/main/java/edu/co/el2/back_ferreteria/Providers/ProviderRepository.java
+++
@@ -1,9 +0,0 @@
-package edu.co.el2.back_ferreteria.Providers;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ProviderRepository extends JpaRepository<ProviderEntity, Integer> {
-    
-}
